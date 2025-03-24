69e316f99d4f3562172526088f7f673a7ff55b1a
DanielBwenge <bwenge1@my.canisius.edu>
2025-03-24 00:17:38 -0400

    3/24 git push origin main

--- src/main/java/edu/canisius/csc213/project1/Card.java
+++ src/main/java/edu/canisius/csc213/project1/Card.java
@@ -3,42 +3,36 @@ package main.java.edu.canisius.csc213.project1;
 import java.util.Objects;
 
 public class Card {
-    private int value;
-    private String suit;
-    private String rank;
-
     public enum Suit {
         HEARTS, DIAMONDS, CLUBS, SPADES
     }
+
     public enum Rank {
         ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
     }
 
-    public Card(int value, String suit, String rank) {
-        this.value = value;
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Suit suit, Rank rank) {
         this.suit = suit;
         this.rank = rank;
     }
 
-    public int getValue() {
-        return value;
-    }
-
-    public String getSuit() {
+    public Suit getSuit() {
         return suit;
     }
 
-    public String getRank() {
+    public Rank getRank() {
         return rank;
     }
 
+    @Override
     public String toString() {
         return rank + " of " + suit;
     }
 
-
-
-    //hadnhelp creating this part 
+    @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
@@ -46,10 +40,8 @@ public class Card {
         return suit == card.suit && rank == card.rank;
     }
 
+    @Override
     public int hashCode() {
         return Objects.hash(suit, rank);
     }
-
-
-
 }
--- src/main/java/edu/canisius/csc213/project1/Deck.java
+++ src/main/java/edu/canisius/csc213/project1/Deck.java
@@ -1,6 +1,5 @@
-package edu.canisius.csc213.project1;
+package main.java.edu.canisius.csc213.project1;
 
-import java.util.Objects;   
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,10 +9,31 @@ public class Deck {
     private final List<Card> cards;
 
     public Deck(int size) { 
-        if ( size <= 0 || size > 52 || size %4 != 0) {
-            throw new IllegalArgumentException("Invalid deck size  Must be a multiple of 4 and <= 52.");
+        if (size <= 0 || size > 52 || size % 4 != 0) {
+            throw new IllegalArgumentException("Invalid deck size. Must be a multiple of 4 and <= 52.");
+        }
+        
+        this.cards = new ArrayList<>(size);
+        int numRanks = size / 4;
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (int i = 0; i < numRanks; i++) {
+                this.cards.add(new Card(suit, Card.Rank.values()[i]));
+            }
         }
     }
 
-    
-}   
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card draw() {
+        if (cards.isEmpty()) {
+            throw new NoSuchElementException("Deck is empty");
+        }
+        return cards.remove(0);
+    }
+
+    public int size() {
+        return cards.size();
+    }
+}
